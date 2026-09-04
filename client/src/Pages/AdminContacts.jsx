5b9e15a9c0de0870f5d7a6e3b5d6fa7384b00bb3
Avinash Upadhyay <avinashupa985@gmail.com>
2026-09-04 20:41:18 +0530

    Update API endpoint for fetching admin contacts

--- client/src/Pages/AdminContacts.jsx
+++ client/src/Pages/AdminContacts.jsx
@@ -9,7 +9,7 @@ const AdminContacts = () => {
 
   const getContactData = async () => {
     try {
-      const response = await axios.get(`http://localhost:5000/api/admin/contacts`, {
+      const response = await axios.get(`https://lms-platform-backend-psal.onrender.com/api/admin/contacts`, {
         headers: {
           Authorization: authorizationToken,
         },
