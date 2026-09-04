3f2284a0112ecf792c682ccbe22f47920fac560b
Avinash Upadhyay <avinashupa985@gmail.com>
2026-09-04 20:30:29 +0530

    Enable error middleware functionality
    Uncomment error middleware function and clean up code.

--- server/middlewares/error-middleware.js
+++ server/middlewares/error-middleware.js
@@ -1,17 +1,4 @@
-// const errorMiddleware =(err,req,res,next)=>{
-//     const status = err.status||500;
-//     // const message = err.message||"Backend Error";
-//     const message = typeof err.message === "string" ? err.message : "Backend Error";
 
-//     // const extraDetails = err.extraDetails||"Error from Backend"
-//     const extraDetails = typeof err.extraDetails === "string" ? err.extraDetails : "Error from Backend";
-
-//     return res.status(status).json({message,extraDetails})
-// }
-
-
-
-// module.exports = errorMiddleware
 const errorMiddleware = (err, req, res, next) => {
     const status = err.status || 500;
     const message = err.message || "Backend Error";
