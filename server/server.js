af6164ec6841fcc7ff0726f185930297a4ee351e
Avinash Upadhyay <avinashupa985@gmail.com>
2026-09-04 20:53:39 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ const errorMiddleware = require("./middlewares/error-middleware")
 const corsOptions = {
     origin: [
         "http://localhost:5173",
-        "https://lms-platform-apii.onrender.com"
+        "https://lms-platform-backend-psal.onrender.com"
     ],
     methods: "GET,POST,PUT,DELETE,PATCH,HEAD",
     credentials: true
